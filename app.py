00b9ca8ea1fc583996f6b8311036e08e1067f45b
Steffen Röcker <sroecker@gmail.com>
2023-11-09 17:53:44 +0100

    Rewrite to use torchvision image transforms

--- app.py
+++ app.py
@@ -2,38 +2,31 @@ import numpy as np
 from requests import post
 from classes import default_class_labels
 import streamlit as st
-import torch
+import torchvision.transforms.functional as fn
+from torchvision.transforms import v2
 from PIL import Image
 
-DEFAULT_SIZE = (224, 224)
+DEFAULT_SIZE = (256, 256)
 
 PREDICTION_URL = 'https://birds-birdwatching.apps.rhods-internal.61tk.p1.openshiftapps.com/v2/models/birds/infer'
 
-def reshape_image_onnx(img_arr):
-    # change to channels first
-    # and add one dimension
-    return np.rollaxis(img_arr, 2)[np.newaxis, :]
-
 def serialize_image(img):
-    img_arr = np.asarray(img, dtype='int32')
+    img_arr = fn.pil_to_tensor(img).unsqueeze(0)
     print(img_arr.shape)
-    img_re = reshape_image_onnx(img_arr)
-    print(img_re.shape)
-    # TODO fix defaults
     payload = {
         'inputs': [
             {
                 'name': 'image_1_3_256_256',
-                'shape': [1, 3, 256, 256],
+                'shape': [1, 3, DEFAULT_SIZE[0], DEFAULT_SIZE[1]],
                 'datatype': 'FP32',
-                'data': img_re.flatten().tolist(),
+                'data': img_arr.flatten().tolist(),
             }
         ]
     }
     return payload
 
 
-st.set_page_config(page_title='Birdwatching')
+st.set_page_config(page_title='RHODS Birdwatching demo')
 
 st.title('Drag and Drop scoring')
 
@@ -41,12 +34,18 @@ img_data = st.file_uploader(label='Upload bird image for scoring', type=['png', 
 if img_data is not None:
     uploaded_img = Image.open(img_data)
     print(uploaded_img.size)
-    uploaded_img = uploaded_img.resize((256, 256), resample=Image.Resampling.BICUBIC)
-    print(uploaded_img.size)
-    st.image(uploaded_img)
-    payload = serialize_image(uploaded_img)
+
+    transform = v2.Compose([v2.Resize(size=DEFAULT_SIZE)])
+    new_img = transform(uploaded_img)
+    print(new_img.size)
+    
+    st.image(new_img)
+    
+    payload = serialize_image(new_img)
     raw_response = post(PREDICTION_URL, json=payload)
     print(raw_response.text)
+    
     res = raw_response.json()
-    pred_class = default_class_labels[np.argmax(res['outputs'][0]['data'])]
+    prediction = res['outputs'][0]['data']
+    pred_class = default_class_labels[np.argmax(prediction)]
     st.write(f'Predicted class: {pred_class}')
